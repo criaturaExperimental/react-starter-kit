bcfa142b2cf01b1673059c7ecb8d86618d4f1823
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-07 14:14:25 +0100

    Gets all sassy

--- webpack.config.js
+++ webpack.config.js
@@ -8,29 +8,35 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-    module: {
-        loaders: [
-           {
-               test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/
-           },
-           {
-               test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/
-           }
-
-        ]
-    },
-    resolve: {
-        modules: [
-          path.resolve(__dirname, 'src/components'),
-          path.resolve(__dirname, 'node_modules')
-        ]
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
       },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ]},
+    resolve: {
+      modules: [
+        path.resolve(__dirname, 'src/components'),
+        path.resolve(__dirname, 'node_modules')
+      ]
+    },
     plugins: [
-        HtmlWebpackPluginConfig
+      HtmlWebpackPluginConfig
     ]
-}
+  }
