d610f262df4b618b2ba3e34dc6f0a25df0f789ba
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-07 11:31:16 +0100

    Atmizes list into items

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import listApi from '../../data/list.json';
 import getList from '../../services/getList';
 
+import Item from 'List/Item.jsx';
+
 export default function List() {
 
     return (
       <ul>
         {getList(listApi).map(function(item, i){
-          return <li key={i}> {item} </li> // should be another component to which i pass the element
+          return <Item key={i} item={item}/>
         })}
       </ul>
     );
