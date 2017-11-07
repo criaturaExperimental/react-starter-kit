446222d424198b5a0d2a1951965f0989524ef0fc
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-07 15:07:53 +0100

    Adds little jibberish

--- src/components/List/Item.jsx
+++ src/components/List/Item.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+const babbling = [
+  'Master', 'Comprehend', 'Grasp',
+  'Understand', 'Expert', 'Get the knack of',
+  'Learn'
+]
+
+const endBabbling = [
+  'in a week', 'in 24 hours', 'in a lifetime', 'in minutes', 'in less than you think'
+]
+
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function pluckTitle() {
+  return {
+    'first': babbling[getRandomArbitrary(0, babbling.length)],
+    'second': endBabbling[getRandomArbitrary(0, endBabbling.length)]
+  }
+}
+
 export default function Item(props) {
   return (
-    <li>{ props.item }</li>
+    <li>{
+      `${pluckTitle().first}
+      ${props.item} breeding ${pluckTitle().second}`
+    }</li>
   )
 }
