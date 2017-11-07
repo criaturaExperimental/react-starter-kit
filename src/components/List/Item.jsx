c454d0a33ea76696d98529747f1e456d1ba7ce80
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-07 15:09:00 +0100

    Refactors from sillyness

--- src/components/List/Item.jsx
+++ src/components/List/Item.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 
-const babbling = [
-  'Master', 'Comprehend', 'Grasp',
-  'Understand', 'Expert', 'Get the knack of',
-  'Learn'
-]
-
-const endBabbling = [
-  'in a week', 'in 24 hours', 'in a lifetime', 'in minutes', 'in less than you think'
-]
-
-function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function pluckTitle() {
-  return {
-    'first': babbling[getRandomArbitrary(0, babbling.length)],
-    'second': endBabbling[getRandomArbitrary(0, endBabbling.length)]
-  }
-}
-
 export default function Item(props) {
   return (
-    <li>{
-      `${pluckTitle().first}
-      ${props.item} breeding ${pluckTitle().second}`
-    }</li>
+    <li>{ props.item }</li>
   )
 }
