3928d01134dc2f23028e84b5ae57ded6c2785466
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-07 09:08:53 +0100

    Gets json list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import listApi from '../data/list.json'
+
+
+function getList(data) {
+  let arrData = [];
+  const myList = listApi.message;
+  for (var key in myList) {
+    if (myList.hasOwnProperty(key)) {
+      arrData.push(key);
+    }
+  }
+  return arrData
+}
 
 export default function App() {
-  function helloWorld() {
-    return "hello world"
-  }
-
 
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>{ helloWorld() }</h1>
-      </div>);
+      <ul>
+        {getList(listApi).map(function(item, i){
+          return <li key={i}> {item} </li>
+        })}
+      </ul>
+    );
 }
