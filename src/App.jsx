e4e1a85a8c6af58fdaff8b6e37d2b35a89448155
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-08 13:03:53 +0100

    Refactors styles sheet application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('./styles/style.scss');
+
+
 import List from 'List/List.jsx';
 import Header from 'Header.jsx';
-import CheckoutBasket from 'CheckoutBasket.jsx'
+import CheckoutBasket from 'CheckoutBasket.jsx';
 
 export default function App() {
   return (
