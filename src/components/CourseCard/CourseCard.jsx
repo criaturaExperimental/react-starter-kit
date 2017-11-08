943a270ca3d9030f949977ef8be66ceafd2997ee
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-08 09:47:44 +0100

    Uses capitalizer

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import pluckTitle from '../../helpers/babbler';
 import pluckPrice from '../../helpers/pricer';
+import capitalizer from '../../helpers/capitalizer';
 
 export default function CourseCard(props) {
   return(
     <li>
       <h4>
-        {`${pluckTitle().first} ${props.item} breeding ${pluckTitle().second}`}
+        {`${pluckTitle().first} ${capitalizer(props.item)} breeding ${pluckTitle().second}`}
       </h4>
       <div className="course-payment">
         <span>{ pluckPrice() }</span>
--- src/helpers/capitalizer.js
+++ src/helpers/capitalizer.js
@@ -1,3 +1,3 @@
-export function capitalizer(word) {
+export default function capitalizer(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
