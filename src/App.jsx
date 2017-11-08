d16dd8d22eb660efb789fccb32caf5f199e78ca3
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-08 13:48:26 +0100

    Renders from general class application

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,13 @@ class App extends React.Component {
 
   render(){
     return (
-      <h1>I'm working</h1>
+      <main>
+        <Header />
+        <List />
+        <CheckoutBasket />
+      </main>
     )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-// export default function App() {
-//   return (
-//     <main>
-//       <Header />
-//       <List />
-//       <CheckoutBasket />
-//     </main>
-//   )
-// }
