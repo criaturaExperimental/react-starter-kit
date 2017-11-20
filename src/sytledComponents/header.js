8bf75357445e37bfc23e7240bcb736b2ae410521
Lucas Gomez <lucas.gomez@lastminute.com>
2017-11-20 10:31:44 +0100

    Cleans header styles

---
+++ src/sytledComponents/header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.header`
+  width: 60vw;
+  border-bottom: 1px solid black;
+  text-align: center;
+  h1{
+    margin-bottom: 2%;
+  }
+`;
